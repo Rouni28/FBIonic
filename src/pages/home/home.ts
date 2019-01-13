e38eea42523ddb4d8bbfe78b8623d060d650dbd8
rouni28 <vasani28.rv@gmail.com>
2019-01-13 17:44:44 +0530

    Added body of fb login page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public texto: any;
+  public dateOfevent:string= new Date().toISOString();
+  fecha () {
+    this.texto =this.dateOfevent.substr(0,10);
+    console.log("Fecha seleccionada ", this.dateOfevent.substr(0,10));
+  }
   constructor(public navCtrl: NavController) {
 
   }
