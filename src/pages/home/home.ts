5d8fa31e5d2948192f8187f612964f6efafd6462
rouni28 <vasani28.rv@gmail.com>
2019-01-15 15:51:43 +0530

    Login page changed for mobile view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,13 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   public texto: any;
+  public mobileDevice : boolean = false;
   public dateOfevent:string= new Date().toISOString();
   fecha () {
     this.texto =this.dateOfevent.substr(0,10);
     console.log("Fecha seleccionada ", this.dateOfevent.substr(0,10));
   }
   constructor(public navCtrl: NavController) {
-
+    this.detectmob();
+    //console.log(this.mobileDevice);
   }
+
+  detectmob() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       this.mobileDevice = true;
+     }
+    else {
+      this.mobileDevice = false;
+     }
+   }
 
 }
